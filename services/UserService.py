ac6188637209137325275d136d6096a27c07d270
Orpheus Peng <stannolite@gmail.com>
2021-06-15 22:06:18 -0700

    Add signup endpoint

--- services/UserService.py
+++ services/UserService.py
@@ -1,12 +1,21 @@
 from models.User import UserDocument
 from typing import List, Union, Optional
+import random
+import string
 
 
 def get_users_from_db() -> List[UserDocument]:
     return UserDocument.objects
 
 
-def populate_user_db_entry(display_name: str, phone_number: str) -> UserDocument:
+def populate_user_db_entry(
+    phone_number: str,
+    display_name: Optional[str] = None
+) -> UserDocument:
+    if not display_name:
+        letters = string.ascii_lowercase
+        display_name = ''.join(random.choice(letters) for i in range(10))
+
     entry = UserDocument(
         display_name=display_name,
         phone_number=phone_number,
@@ -27,3 +36,8 @@ def _get_user_by_phone_number(phone_number: str) -> Optional[UserDocument]:
 
 def validate_user_login(phone_number: str) -> Optional[UserDocument]:
     return _get_user_by_phone_number(phone_number)
+
+
+def update_user_display_name(user_id: str, display_name: str):
+    user = get_user_entry(user_id)
+    user.update(set__display_name=display_name)
