0c4cddf5ba9800d7ad6e04c0d1ede845093c6873
Orpheus Peng <stannolite@gmail.com>
2021-06-15 20:34:07 -0700

    Add utility functions for getting a user by phone number

--- services/UserService.py
+++ services/UserService.py
@@ -1,5 +1,5 @@
 from models.User import UserDocument
-from typing import List, Union
+from typing import List, Union, Optional
 
 
 def get_users_from_db() -> List[UserDocument]:
@@ -18,3 +18,12 @@ def populate_user_db_entry(display_name: str, phone_number: str) -> UserDocument
 def get_user_entry(user_id: str) -> Union[type(None), UserDocument]:
     entry = UserDocument.objects(resource_code=user_id).first()
     return entry
+
+
+def _get_user_by_phone_number(phone_number: str) -> Optional[UserDocument]:
+    entry = UserDocument.objects(phone_number=phone_number).first()
+    return entry
+
+
+def validate_user_login(phone_number: str) -> Optional[UserDocument]:
+    return _get_user_by_phone_number(phone_number)
