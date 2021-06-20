fed6104ca0d46687c5a43607d4df60036bca620f
Orpheus Peng <stannolite@gmail.com>
2021-06-19 17:56:44 -0700

    The script seems to requires su access. Updated.

--- scripts/finish_install.sh
+++ scripts/finish_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 -m venv $HOME/group_act_backend/venv
-source $HOME/group_act_backend/venv/bin/activate
-pip3 install -r $HOME/group_act_backend/requirements.txt
+sudo python3 -m venv $HOME/group_act_backend/venv
+sudo source $HOME/group_act_backend/venv/bin/activate
+sudo pip3 install -r $HOME/group_act_backend/requirements.txt
