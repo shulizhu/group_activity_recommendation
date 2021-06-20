f2f8219654f0c95cb499ea6cdfab29ab92f38f16
Orpheus Peng <stannolite@gmail.com>
2021-06-19 18:22:23 -0700

    Minor change to finish_install.sh script

--- scripts/finish_install.sh
+++ scripts/finish_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$BASE=$HOME/group_act_backend
-$VENV=$BASE/venv
+BASE=$HOME/group_act_backend
+VENV=$BASE/venv
 
 sudo python3 -m venv $VENV
 sudo $VENV/bin/pip3 install -r -r $BASE/requirements.txt
