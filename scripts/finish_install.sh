4d0f6b86600a785b14aed764e5e558b5610199f0
Orpheus Peng <stannolite@gmail.com>
2021-06-27 01:04:02 -0700

    Refine finish_install script

--- scripts/finish_install.sh
+++ scripts/finish_install.sh
@@ -5,4 +5,5 @@ VENV=$BASE/venv
 
 sudo python3 -m venv $VENV
 sudo $VENV/bin/pip3 install -r $BASE/requirements.txt
+sudo rm -rf $BASE/group_act_backend/build
 sudo aws s3 cp s3://group-act $BASE/group_act_backend/build --recursive
